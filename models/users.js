fb0be66108e2e0242d370d94339f0568f910391c
ilzicode <63634401+ilzicode@users.noreply.github.com>
2022-08-20 18:38:44 +0700

    fix auto increment migration

--- models/users.js
+++ models/users.js
@@ -7,7 +7,7 @@ module.exports =(Sequelize, DataTypes)=>{
 
             id: {
                 type: DataTypes.INTEGER,
-                autoIncreament: true,
+                autoIncrement: true,
                 primaryKey: true
             },
 
